adb4c4e3aa27fabbf0655a2a78f56afc9c0cf68d
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 01:21:52 -0700

    MoodAnalyser.cs

---
+++ MoodAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Moodanalyser
+    {
+        public string message = "I am in Sad Mood";
+        public string AnalyseMood(string msg)
+        {
+            if (this.message == msg)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+    }
+}
